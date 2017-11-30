17d3e80918c8cb486e2afa06f1d66f0315072a85
“dihtyar” <bogdandihtyar@gmail.com>
2017-11-30 15:18:56 +0200

    control element

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from './components/Header';
+import Todo from './components/Todo';
+import Form from './components/Form';
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            todos: this.props.initialData
+        };
+
+        this.handleStatusChange = this.handleStatusChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    nextId(){
+        this._nextId = this._nextId || 4;
+        return this._nextId++
+    }
+
+    handleStatusChange(id) {
+        let todos = this.state.todos.map(todo =>{
+           if(todo.id === id){
+               todo.completed = !todo.completed;
+           }
+           return todo;
+        });
+        this.setState({todos});
+    }
+
+    handleDelete(id){
+        let todos = this.state.todos.filter(todo => todo.id !== id);
+        this.setState({todos});
+    }
+
+    handleAdd(title){
+        let todo ={
+            id: this.nextId(),
+            title,
+            completed: false
+        };
+        let todos = [...this.state.todos, todo];
+        this.setState({todos});
+    }
+
+    render(){
+        return (
+            <main>
+                <Header title={this.props.title} todos={this.state.todos}/>
+                <section className="todo-list">
+                    {this.state.todos.map(todo =>
+                        <Todo
+                            key={todo.id}
+                            id={todo.id}
+                            title={todo.title}
+                            completed={todo.completed}
+                            onStatusChange={this.handleStatusChange}
+                            onDelete={this.handleDelete}
+                        />
+                    )}
+                </section>
+                <Form onAdd={this.handleAdd}/>
+            </main>
+        );
+    }
+}
+
+App.propTypes = {
+    title: PropTypes.string,
+    initialData: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired
+    })).isRequired
+};
+
+App.defaultProps ={
+    title: 'React Todo'
+};
+
+
+export default App;
+
