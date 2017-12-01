79c22634983859cbc2c02bddaad9a25949798f4a
“dihtyar” <bogdandihtyar@gmail.com>
2017-12-01 09:38:28 +0200

    dont control element add edit form

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,15 +4,31 @@ import PropTypes from 'prop-types';
 import Checkbox from './Checkbox';
 import Button from './Button';
 
-function Todo(props){
+class Todo extends React.Component{
 
-    return (
-        <div className={`todo${props.completed ? ' completed' : ''}`}>
-            <Checkbox checked={props.completed} onChange = {()=> props.onStatusChange(props.id)} />
-            <span className="todo-title">{props.title}</span>
-            <Button className="delete icon" icon="delete" onClick={()=> props.onDelete(props.id)}/>
-        </div>
-    );
+    constructor(props){
+        super(props);
+        this.state = {
+            editing: true
+        }
+    }
+
+    render(){
+        return(this.state.editing ?
+                <form className="todo-edit-form"  onSubmit={this.handleSubmit}>
+                    <input type="text" ref="title" defaultValue={this.props.title}/>
+                    <Button className="save icon" icon="save" type="submit" />
+                </form>
+                :
+                <div className={`todo${this.props.completed ? ' completed' : ''}`}>
+                    <Checkbox checked={this.props.completed} onChange = {()=> this.props.onStatusChange(this.props.id)} />
+                    <span className="todo-title">{this.props.title}</span>
+                    <Button className="edit icon" icon="edit" />
+                    <Button className="delete icon" icon="delete" onClick={()=> this.props.onDelete(this.props.id)}/>
+                </div>
+        );
+    }
+
 }
 
 Todo.propTypes = {
